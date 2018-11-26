569d56d53f60f88bb013f06e5acc31ca23b410f5
Jake Gealer <jake@gealer.email>
2018-11-26 17:08:24 +0000

    Made a switch

--- parsethoseargs/c_parser.c
+++ parsethoseargs/c_parser.c
@@ -12,9 +12,6 @@ typedef struct {
 // The structure for the argument parser.
 
 static ArgParserStructure ArgParser_Next(ArgParserStructure structure) {
-    char Quote = '"';
-    // Defines a quote.
-
     char Space = ' ';
     // Defines a space.
 
@@ -34,35 +31,32 @@ static ArgParserStructure ArgParser_Next(ArgParserStructure structure) {
     char* *parsing_str = &structure.ParsingString;
     char current_char = *parsing_str[0];
     while (current_char != '\0') {
-        if (current_char == Quote) {
-            // This is a quote.
-            if (InQuotes == 1) {
-                // This is the end of some quoted text.
-                ++*parsing_str;
-                goto parse_done;
-            } else {
-                // How do we handle the quote?
-                if (structure.IgnoreQuotes) {
-                    CurrentParse[(int) strlen(CurrentParse)] = current_char;
-                } else {
-                    InQuotes = 1;
-                }
-            }
-        } else if (current_char == Space) {
-            // This is a space.
-            if (InQuotes == 0) {
-                // Probably count this as a break.
-                if (!(strcmp(CurrentParse, "") == 0)) {
+        switch (current_char) {
+            case '"':
+                if (InQuotes == 1) {
+                    // This is the end of some quoted text.
                     ++*parsing_str;
                     goto parse_done;
                 }
-            } else {
+
+                // Are we ignoring quotes?
+                if (!structure.IgnoreQuotes) {
+                    InQuotes = 1;
+                    break;
+                }
+            case ' ':
+                if (InQuotes == 0) {
+                    // Probably count this as a break.
+                    if (strcmp(CurrentParse, "") == 0) {
+                        break;
+                    } else {
+                        ++*parsing_str;
+                        goto parse_done;
+                    }
+                }
+            default:
                 // Add this to the quote text.
                 CurrentParse[(int) strlen(CurrentParse)] = current_char;
-            }
-        } else {
-            // Add this to the text.
-            CurrentParse[(int) strlen(CurrentParse)] = current_char;
         }
 
         ++*parsing_str;
